5147873373dd1940b3bb4d6842381000dfe1dc6c
metin90 <mtnhsdmr@gmail.com>
2021-10-02 17:55:25 +0300

    path of nodejs changed in Data.java

--- src/main/java/GeneralFiles/Data.java
+++ src/main/java/GeneralFiles/Data.java
@@ -3,7 +3,7 @@ package GeneralFiles;
 
 public class Data {
 
-    public static String path_NodeJS_mac= System.getProperty("user.dir")+"/nodeJS/bin/node";
+    public static String path_NodeJS_mac="/usr/local/bin/node";// System.getProperty("user.dir")+"/nodeJS/bin/node";
     public static String path_NodeJS_Win=System.getenv("ProgramFiles")+"\\nodejs\\node.exe";
     public static String filesPath=System.getProperty("user.dir")+"/Files";
     public static String pomXMLPath=System.getProperty("user.dir")+"/pom.xml";
